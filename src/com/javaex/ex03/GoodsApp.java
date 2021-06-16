f3359d2aa57779a58420cd569914b12efa5ef6b7
wonho <wonho951@gmail.com>
2021-06-16 18:10:52 +0900

    2021.06.16 ArrayList 과제

--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,11 +1,22 @@
 package com.javaex.ex03;
 
+import java.util.Scanner;
+
 public class GoodsApp {
 
     public static void main(String[] args) {
 
     	
     	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	
+    	
+    	System.out.println("상품을 입력해주세요(종료q)");
+    	
+    	
+    	
+    	sc.close();
     	
        
     }
