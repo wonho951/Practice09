48ea1509a3f8a23410477e4e32cf0308819b79c5
wonho <wonho951@gmail.com>
2021-06-17 16:49:51 +0900

    2021.06.16 ArrayList 과제

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,7 +1,10 @@
 package com.javaex.ex02;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+
 
 public class FriendApp {
 
@@ -9,32 +12,37 @@ public class FriendApp {
 
     	
         
+    	
         Scanner sc = new Scanner(System.in);
-        
-        
-        ArrayList<Friend> fArray = new ArrayList<Friend>();
-        
+
+        List<Friend> fArray = new ArrayList<Friend>();	//친구3명 담을 그릇 준비
         System.out.println("친구를 3명 등록해 주세요");
 
-        //친구정보 3명 입력 로직 --> 반복문 사용
+        try {
+        	//친구정보 3명 입력 로직 --> 반복문 사용
+            for( int i = 0; i < 3; i++) {    
+            	// 친구정보(이름,핸드폰,학교) 입력받기
+            	String Info = sc.nextLine();
+            	String[] sArray = new String[3];//3명 담을 방 만들고
+            	sArray = Info.split(" ");
+      
+            	
+                // Friend 인스턴스 생성하여 데이터 넣기
+            	Friend info = new Friend();
+            	
+            	info.setName(sArray[0]);
+            	info.setHp(sArray[1]);
+            	info.setSchool(sArray[2]);        	        	
+                // 인스턴스 주소를 배열에 대입
+            	
+            	fArray.add(info);
+            }
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("다시 입력해주세요.");
+		}
         
-        for( int i = 0; i < fArray.size(); i++) {    
-        	
-        	// 친구정보(이름,핸드폰,학교) 입력받기
-        	
-        	String Info = sc.nextLine();
-        	String[] sArray = Info.split(" ");
-  
-        	
-            // Friend 인스턴스 생성하여 데이터 넣기
-        	
-        	
-        	    	        	
-            // 인스턴스 주소를 배열에 대입
-        	
-        	
-        }
-        
+                
         
         // 친구정보 출력
         for (int i = 0; i < fArray.size(); i++) {
@@ -43,8 +51,6 @@ public class FriendApp {
         }
 
         sc.close();
-    	
-    	
     }
 
 }
